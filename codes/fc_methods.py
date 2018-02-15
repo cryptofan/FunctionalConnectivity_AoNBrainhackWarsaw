8b3fc562d578add96f24f922b556417fa9c904e7
AleksanderMolak <aleksander.molak@gmail.com>
2018-02-15 16:38:43 +0100

    Methods library
    An importable library with all the methods implemented: partial_corr(X), euclidean_distance(x, y), manhattan_distance(x, y), calc_MI(x, y, n_bins)

---
+++ codes/fc_methods.py
@@ -0,0 +1,84 @@
+# This file is intended to work as a library.
+# you can
+# import fc_methods as fc
+# and then e.g.:
+# fc.manhattan_distance(x,y)
+# etc.
+# Happy coding! :)
+
+
+# Import dependencies
+from scipy import stats, linalg
+import numpy as np
+
+# Define methods
+def partial_corr(C):
+    """
+    Returns the sample linear partial correlation coefficients between pairs of variables in C, controlling
+    for the remaining variables in C.
+    Parameters
+    ----------
+    C : array-like, shape (n, p)
+        Array with the different variables. Each column of C is taken as a variable
+    Returns
+    -------
+    P : array-like, shape (p, p)
+        P[i, j] contains the partial correlation of C[:, i] and C[:, j] controlling
+        for the remaining variables in C.
+    """
+
+    C = np.asarray(C)
+    p = C.shape[1]
+    P_corr = np.zeros((p, p), dtype=np.float)
+    for i in range(p):
+        P_corr[i, i] = 1
+        for j in range(i+1, p):
+            idx = np.ones(p, dtype=np.bool)
+            idx[i] = False
+            idx[j] = False
+            beta_i = linalg.lstsq(C[:, idx], C[:, j])[0]
+            beta_j = linalg.lstsq(C[:, idx], C[:, i])[0]
+
+            res_j = C[:, j] - C[:, idx].dot( beta_i)
+            res_i = C[:, i] - C[:, idx].dot(beta_j)
+
+            corr = stats.pearsonr(res_i, res_j)[0]
+            P_corr[i, j] = corr
+            P_corr[j, i] = corr
+
+    return P_corr
+
+
+def euclidean_distance(x,y):
+    """Returns euclidean distance between two lists or numpy arrays"""
+    x = np.array(x)
+    y = np.array(y)
+    return np.sqrt(sum((x-y)**2))
+
+
+def manhattan_distance(x,y):
+    """Returns manhattan distance between two lists or numpy arrays"""
+    x = np.array(x)
+    y = np.array(y)
+    return sum(abs(x - y))
+
+
+def calc_MI(X,Y,bins):
+   """Returns Shannon's mutual information between two array-like objects"""
+   c_XY = np.histogram2d(X,Y,bins)[0]
+   c_X = np.histogram(X,bins)[0]
+   c_Y = np.histogram(Y,bins)[0]
+
+   H_X = shan_entropy(c_X)
+   H_Y = shan_entropy(c_Y)
+   H_XY = shan_entropy(c_XY)
+
+   MI = H_X + H_Y - H_XY
+   return MI
+
+def shan_entropy(c):
+    """Retuurns Shannon's information (entropy) for arrray-like object"""
+    c_normalized = c / float(np.sum(c))
+    c_normalized = c_normalized[np.nonzero(c_normalized)]
+    H = -sum(c_normalized* np.log2(c_normalized))
+    return H
